ab9f6aa81b3a167a62979069863dbc2815bceefe
helinlin <964464328@qq.com>
2015-12-11 00:21:31 +0800

    add the intentTest demo

---
+++ intentTest/src/com/example/intenttest/MainActivity.java
@@ -0,0 +1,50 @@
+package com.example.intenttest;
+
+import android.support.v7.app.ActionBarActivity;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends ActionBarActivity {
+	Button turnButton;
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		turnButton = (Button)findViewById(R.id.button1);
+		turnButton.setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View arg0) {
+				// TODO Auto-generated method stub
+				Intent tempIntent = new Intent(Intent.ACTION_VIEW);
+				tempIntent.addCategory("android.intent.category.MYCATEGOTY");
+				tempIntent.setData(Uri.parse("http://www.baidu.com"));
+				startActivity(tempIntent);
+			}
+		});
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// Handle action bar item clicks here. The action bar will
+		// automatically handle clicks on the Home/Up button, so long
+		// as you specify a parent activity in AndroidManifest.xml.
+		int id = item.getItemId();
+		if (id == R.id.action_settings) {
+			return true;
+		}
+		return super.onOptionsItemSelected(item);
+	}
+}
---
+++ intentTest/src/com/example/intenttest/secondactivity.java
@@ -0,0 +1,14 @@
+package com.example.intenttest;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class secondactivity extends Activity{
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.secondactivity);
+	}
+
+}
